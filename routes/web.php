991742ed0da8bb34d24afab8cdab8dfd2a26fac1
takanami_madoka <takanami.mm@gmail.com>
2023-12-10 14:01:07 +0900

    feat:controllerとrouteの作成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tweets', \App\Http\Controllers\Tweets\IndexController::class);
+Route::get('/tweets/{id}', \App\Http\Controllers\Tweets\ShowController::class);
